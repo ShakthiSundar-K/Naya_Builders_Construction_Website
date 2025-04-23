e738d6d714fd1ee03117f7882bcdb08e2025a806
ShakthiSundar-K <shakthisundar01@gmail.com>
2025-04-23 09:49:33 +0530

    implement contact hero section

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ContactSection from "../components/ContactSection";
+import ContactHeroSection from "../components/ContactHeroSection";
 
 const Contact = () => {
   return (
-    <>
+    <div className='mt-20'>
+      <ContactHeroSection />
       <ContactSection />
-    </>
+    </div>
   );
 };
 
